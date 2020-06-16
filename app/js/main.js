e3ceaa12388a4eaedaeda5db92f3172a8f0d1b3f
lismgmk <lismgmk2@gmail.com>
2020-06-17 01:11:16 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,31 @@ $(function(){
           arrows: false,
           slidesToShow: 4,
           slidesToScroll: 4,
+          responsive: [
+            {
+              breakpoint: 1900,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                }
+              },
+              {
+                breakpoint: 801,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+          ]
       });
 
       $(".js-range-slider").ionRangeSlider({
@@ -39,11 +64,13 @@ $(function(){
         $('.menu__list').slideToggle();
    });
    
-   
+  
 
    $('.header__btn-menu').on('click', function(){
     $('.header__box').toggleClass('active');
 });
+
+$('input[type="file"], select').styler();
 
 
 $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function(event) {
